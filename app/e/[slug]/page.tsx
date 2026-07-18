4f20c5fa708354200655f5a11c6c2022b16d9237
LucasP2001 <lucaspinheiro.tapaua2@gmail.com>
2026-07-17 22:23:57 -0400

    style: remove aviso "ingresso por e-mail" da pagina do evento

--- app/e/[slug]/page.tsx
+++ app/e/[slug]/page.tsx
@@ -133,9 +133,6 @@ export default async function EventoPublicoPage({ params }: { params: { slug: st
           </>
         )}
         <Cronograma dias={ev.dias ?? []} contagens={await contarPorSessao(createAdminSupabase(), ev.id)} />
-        <p className="text-center text-xs text-muted mt-6">
-          Você receberá um ingresso digital com QR code por e-mail.
-        </p>
         <RecuperarAcesso slug={ev.slug} />
       </div>
 
